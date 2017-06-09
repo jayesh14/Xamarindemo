e1814237fba71fe094bd2dc2d14ab6612437c8f3
amit.kasundra <amit.kasundra@tatvasoft.com>
2017-06-09 18:13:00 +0530

    Added

---
+++ FirstApp/FirstApp/FirstApp/DemoPageMaster.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DemoPageMaster : ContentPage
+    {
+        public ListView ListView => ListViewMenuItems;
+
+        public DemoPageMaster()
+        {
+            InitializeComponent();
+            BindingContext = new DemoPageMasterViewModel();
+        }
+
+
+
+        class DemoPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<DemoPageMenuItem> MenuItems { get; }
+            public DemoPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<DemoPageMenuItem>(new[]
+                {
+                    new DemoPageMenuItem { Id = 0, Title = "Page 1" },
+                    new DemoPageMenuItem { Id = 1, Title = "Page 2" },
+                    new DemoPageMenuItem { Id = 2, Title = "Page 3" },
+                    new DemoPageMenuItem { Id = 3, Title = "Page 4" },
+                    new DemoPageMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        }
+    }
+}
---
+++ FirstApp/FirstApp/FirstApp/DemoPageMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+
+    public class DemoPageMenuItem
+    {
+        public DemoPageMenuItem()
+        {
+            TargetType = typeof(DemoPageDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
